2058b261d5f53537b518a44ca48c50eb4384c7b1
anzueto <jose@C4-1152.local>
2020-02-27 17:15:35 -0500

    h3 update

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -60,7 +60,7 @@ export default class ExercisesList extends Component {
     render() {
         return (
             <div>
-                <h3>Logged Exercises change</h3>
+                <h3>Logged Exercises</h3>
                 <table className="table">
                     <thead className="thead-ligh">
                         <tr>
